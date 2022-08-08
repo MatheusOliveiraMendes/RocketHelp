4ef317536e6807264c6237e012b240b9c964e8fa
Matheus Mendes <matheusmendes@MacBook-Pro-M1-Pro-de-Matheus.local>
2022-08-08 19:15:36 -0300

    stage02

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import { NativeBaseProvider } from "native-base";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
+
+import { THEME } from "./src/styles/theme";
+
+import { Routes } from "./src/routes";
+import { Loading } from "./src/components/Loading";
+
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+
+
+      {fontsLoaded ? <Routes /> : <Loading />}
+
+    </NativeBaseProvider>
+
+
+
+  );
+}
+
+
+
